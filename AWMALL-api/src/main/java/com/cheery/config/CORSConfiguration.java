bd523c74ac0f728c81c649d89a6756cf236a2cce
Cristiano <2281572856@qq.com>
2019-03-14 21:57:54 +0800

    解决前端跨域请求

---
+++ AWMALL-api/src/main/java/com/cheery/config/CORSConfiguration.java
@@ -0,0 +1,34 @@
+package com.cheery.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @desc: 解决客户端跨域请求
+ * @className: CORSConfiguration
+ * @author: RONALDO
+ * @date: 2019-03-14 19:57
+ */
+@Configuration
+public class CORSConfiguration {
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.setAllowCredentials(true);
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+
+}
